db109992018a1e09e1c5ffaad53930f1e456b351
yidayo <418680861@qq.com>
2019-06-10 14:14:44 +0800

    1.1.8

--- rrtf-lesson/src/main/java/org/rrtf/lesson/controller/LessonDataController.java
+++ rrtf-lesson/src/main/java/org/rrtf/lesson/controller/LessonDataController.java
@@ -11,95 +11,110 @@ import java.nio.file.Paths;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.rrtf.lesson.entity.LessonData;
+import org.rrtf.lesson.entity.Member;
+import org.rrtf.lesson.entity.Teacher;
+import org.rrtf.lesson.entity.User;
 import org.rrtf.lesson.mapper.LessonDataRepository;
+import org.rrtf.lesson.mapper.MemberRepository;
 import org.rrtf.lesson.mapper.PubLessonRepository;
-import org.rrtf.lesson.service.RoleTools;
+import org.rrtf.lesson.mapper.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.alibaba.fastjson.JSONObject;
+
 @RestController
 @RequestMapping("/lessonData")
-public class LessonDataController {//上传与下载
+public class LessonDataController {// 上传与下载
 
 	@Autowired
 	LessonDataRepository lessonDataRepository;
-	
+
 	@Autowired
 	PubLessonRepository lessonRepository;
+	@Autowired//查找teacher表用的,被调用了,暂时先放这里
+	TeacherRepository teacherRepository;
+	@Autowired//查找teacher表用的,被调用了,暂时先放这里
+	MemberRepository memberRepository;
 	
-	@Autowired
-	RoleTools tools;
+	@Autowired//redis
+    private RedisTemplate redisTemplate;
 
 	@Value("${upload.location}")
-	String UPLOAD_LOCATION;//从属性文件中读取上传文件最终保存的位置
+	String UPLOAD_LOCATION;// 从属性文件中读取上传文件最终保存的位置
 	@Value("${download.location}")
-	String DOWNLOAD_LOCATION;//从属性文件中读取上传文件最终保存的位置
+	String DOWNLOAD_LOCATION;// 从属性文件中读取上传文件最终保存的位置
 
 	@RequestMapping("/findByLessonId") // 显示所有资料:http://localhost:8080/lessonData/findByLessonId
 	public List<LessonData> findByLessonId(int lessonId) {
 		return lessonDataRepository.findByLessonId(lessonId);
 	}
-	
-	//文件上传
-	@PostMapping("/uploadFile") //上传资料:http://localhost:8080/lessonData/uploadFile
+
+	// 文件上传
+	@PostMapping("/uploadFile") // 上传资料:http://localhost:8080/lessonData/uploadFile
 	public String uploadFile(HttpServletRequest request) throws IOException {
-		//↓权限检测
+		// ↓权限检测
 		int lessonId = Integer.parseInt(request.getParameter("lessonId"));
-		int lessonTeacherId = lessonRepository.findByLessonIdAndLessonStatus(lessonId,1).getTeacherId();
-		int thisTeacherId = tools.getTeacherId();
-		if(thisTeacherId!=lessonTeacherId) return "你不能胡乱给别人的课程上传资料";
-		//↑权限检测
-		//↓获取路径
+		int lessonTeacherId = lessonRepository.findByLessonIdAndLessonStatus(lessonId, 1).getTeacherId();
+		int thisTeacherId = getTeacherId(request);
+		if (thisTeacherId != lessonTeacherId)
+			return "你不能胡乱给别人的课程上传资料";
+		// ↑权限检测
+		// ↓获取路径
 		String urlPath = Thread.currentThread().getContextClassLoader().getResource("").getPath().substring(1);
-		//↑获取路径
-		//↓处理上传的文件,dataName是原文件名,fileName是保存时的文件名
+		// ↑获取路径
+		// ↓处理上传的文件,dataName是原文件名,fileName是保存时的文件名
 		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
 		MultipartFile file = multipartRequest.getFile("lessonData");
 		LessonData data = new LessonData();
 		if (file.isEmpty()) {
 			return "文件为空,原因未知";
 		}
-		byte[] bytes = file.getBytes();//获取上传文件的字节流
-		String dataName = file.getOriginalFilename();//获取上传文件名称
-		int beginIndex = dataName.length()-10<0?0:dataName.length()-10;
-		String fileName = "LessonData"+String.valueOf(new Date().getTime())+dataName.substring(beginIndex);
-		Path path = Paths.get(urlPath,UPLOAD_LOCATION, fileName);//定义保存路径nio包
-		Files.write(path, bytes);//写入最终保存的位置
+		byte[] bytes = file.getBytes();// 获取上传文件的字节流
+		String dataName = file.getOriginalFilename();// 获取上传文件名称
+		int beginIndex = dataName.length() - 10 < 0 ? 0 : dataName.length() - 10;
+		String fileName = "LessonData" + String.valueOf(new Date().getTime()) + dataName.substring(beginIndex);
+		Path path = Paths.get(urlPath, UPLOAD_LOCATION, fileName);// 定义保存路径nio包
+		Files.write(path, bytes);// 写入最终保存的位置
 		data.setDataName(dataName);
 		data.setFileName(fileName);
 		data.setLessonId(Integer.parseInt(request.getParameter("lessonId")));
 		lessonDataRepository.save(data);
-		//↑处理上传的文件,dataName是原文件名,fileName是保存时的文件名
+		// ↑处理上传的文件,dataName是原文件名,fileName是保存时的文件名
 		return "文件上传成功,请不要重复上传";
 	}
-	
-	//文件列表查看:localhost:8080/lessonData/showFiles?lessonId=20
+
+	// 文件列表查看:localhost:8080/lessonData/showFiles?lessonId=20
 	@RequestMapping("/showFiles")
 	private List<LessonData> showFiles(int lessonId) {
 		return lessonDataRepository.findByLessonId(lessonId);
 	}
 
-	//文件下载:localhost:8080/lessonData/downloadFile?dataName=1.txt
+	// 文件下载:localhost:8080/lessonData/downloadFile?dataName=1.txt
 	@RequestMapping("/downloadFile")
-	private String downloadFile(HttpServletResponse response,String dataName) {
-		//↓获取路径
+	private String downloadFile(HttpServletResponse response, String dataName) {
+		// ↓获取路径
 		String urlPath = Thread.currentThread().getContextClassLoader().getResource("").getPath().substring(1);
-		//↑获取路径
-		String downloadFilePath = DOWNLOAD_LOCATION;//被下载的文件在服务器中的路径
+		// ↑获取路径
+		String downloadFilePath = DOWNLOAD_LOCATION;// 被下载的文件在服务器中的路径
 		String fileName = lessonDataRepository.findDataName(dataName);
-		File file = new File(urlPath+downloadFilePath+fileName);
-		System.out.println(urlPath+downloadFilePath+fileName);//测试
+		File file = new File(urlPath + downloadFilePath + fileName);
+		System.out.println(urlPath + downloadFilePath + fileName);// 测试
 		if (file.exists()) {
-			response.setContentType("application/force-download");//设置强制下载不打开
+			response.setContentType("application/force-download");// 设置强制下载不打开
 			response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);
 			byte[] buffer = new byte[1024];
 			FileInputStream fis = null;
@@ -134,5 +149,121 @@ public class LessonDataController {//上传与下载
 			}
 		}
 		return "下载失败";
+	}
+
+	// cookie,redis和session相关
+	// 基础,从cookie中获取String
+	private String readCookie(String name, HttpServletRequest request) {
+		// System.out.println(response+"-----"+request);
+		Cookie[] cookies = request.getCookies();
+		if (null != cookies) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals(name)) {
+					System.out.println("cookie的值:" + cookie.getValue());
+					return cookie.getValue();
+				}
+			}
+		}
+		return null;
+	}
+
+	// 基础,从redis中获取user
+	private User getUser(String username) {
+		ValueOperations opsForValue = redisTemplate.opsForValue();
+		JSONObject object = (JSONObject) opsForValue.get(username);
+		if (object != null) {
+			User user = object.toJavaObject(User.class);
+			System.out.println("user--" + user);
+			return user;
+			// return ((JSONObject)
+			// redisTemplate.opsForValue().get(username)).toJavaObject(User.class);
+		}
+		return null;
+	}
+
+	// 基础,获取userId
+	private int getUserId(HttpServletRequest request) {
+		// addCookie();//创建cookie,测试用
+		String username = readCookie("name", request);// 从cookie中获取-username为"user"
+		if (username != null) {
+			User user = getUser(username);
+			if (user != null) {
+				System.out.println("userId" + user.getUserId());
+				return user.getUserId();// 从redis中获取-userId为2
+			}
+		}
+		return -1;
+	}
+
+	// 获取session中的教师信息,没有则尝试从数据库中查询,并添加到session中
+	private Teacher getSessionTeacher(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		Teacher teacher = (Teacher) session.getAttribute("teacher");
+		if (teacher == null) {
+			int userId = getUserId(request);
+			teacher = teacherRepository.findByUserId(userId);
+			if (teacher != null) {
+				session.setAttribute("teacher", teacher);
+			}
+		}
+		System.out.println("teacher--" + teacher);
+		return teacher;
+	}
+
+	// 获取session中的会员信息,没有则尝试从数据库中查询,并添加到session中
+	private Member getSessionMember(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		Member member = (Member) session.getAttribute("member");
+		if (member == null) {
+			int userId = getUserId(request);
+			member = memberRepository.findByUserId(userId);
+			if (member != null)
+				session.setAttribute("member", member);
+		}
+		System.out.println("member--" + member);
+		return member;
+	}
+
+	// 偷懒,获取teacherId
+	public int getTeacherId(HttpServletRequest request) {
+		if (isLanding(request)) {
+			Teacher teacher = getSessionTeacher(request);
+			if (teacher != null) {
+				System.out.println("teacherId" + teacher.getTeacherId());
+				return teacher.getTeacherId();
+			}
+		}
+		return -1;
+	}
+
+	// 偷懒,获取memberId
+	public int getMemberId(HttpServletRequest request) {
+		Member member = getSessionMember(request);
+		if (isLanding(request)) {
+			if (member != null) {
+				System.out.println("memberId" + member.getMemberId());
+				return member.getMemberId();
+			}
+		}
+		return -1;
+	}
+
+	// 偷懒,看是否登陆(这里的name后期要改)
+	public boolean isLanding(HttpServletRequest request) {
+		// addCookie();//创建cookie,临时方法
+		String username = readCookie("user", request);// 从cookie中获取-username为"user"
+		System.out.println(username != null && getUser(username) != null);
+		return username != null && getUser(username) != null;// 两个都非空则处于登录状态
+	}
+
+	// 偷懒,看是否管理员(这里的name2后期要改)
+	public boolean isAdmin(HttpServletRequest request) {
+		// addCookie2();//创建cookie,临时方法
+		String adminname = readCookie("user", request);
+		System.out.println("adminname" + adminname);
+		System.out.println("getUser" + getUser(adminname));
+		boolean isAdmin = adminname != null && getUser(adminname) != null;
+		System.out.println("是管理员吗?" + isAdmin);
+		return isAdmin;// 两个都非空是管理员
 	}
 }
--- rrtf-lesson/src/main/java/org/rrtf/lesson/controller/PubLessonController.java
+++ rrtf-lesson/src/main/java/org/rrtf/lesson/controller/PubLessonController.java
@@ -8,44 +8,57 @@ import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.rrtf.lesson.entity.Member;
 import org.rrtf.lesson.entity.PubLesson;
+import org.rrtf.lesson.entity.Teacher;
+import org.rrtf.lesson.entity.User;
+import org.rrtf.lesson.mapper.MemberRepository;
 import org.rrtf.lesson.mapper.MyLessonsRepository;
 import org.rrtf.lesson.mapper.PubLessonRepository;
-import org.rrtf.lesson.service.RoleTools;
+import org.rrtf.lesson.mapper.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.alibaba.fastjson.JSONObject;
+
 @RestController
 @RequestMapping("/pubLesson")
 public class PubLessonController {//留着
-	
-	@Autowired
-	RoleTools tools;
 	
 	@Value("${upload.location}")
 	String UPLOAD_LOCATION;// 从属性文件中读取上传文件最终保存的位置
 	
 	@Autowired
 	PubLessonRepository lessonRepository;
-	
 	@Autowired//查找mylessons中间表用的,被调用了,暂时先放这里
 	MyLessonsRepository myLessonsRepository;
+	@Autowired//查找teacher表用的,被调用了,暂时先放这里
+	TeacherRepository teacherRepository;
+	@Autowired//查找teacher表用的,被调用了,暂时先放这里
+	MemberRepository memberRepository;
+	
+	@Autowired//redis
+    private RedisTemplate redisTemplate;
 	
 	//发布:localhost:8080/pubLesson/release(将数据填入后发送到这里
 	@RequestMapping("/release")
 	public String release(HttpServletRequest request) throws IOException {
-		int teacherId = tools.getTeacherId();
+		int teacherId = this.getTeacherId(request);
 		if(teacherId==-1) {
 			return "不是老师不允许创建课程";
 		}
@@ -94,9 +107,9 @@ public class PubLessonController {//留着
 	
 	//删除:localhost:8080/pubLesson/delete?lessonId=1
 	@RequestMapping("/delete")
-	public String delete(int lessonId) {
+	public String delete(int lessonId,HttpServletRequest request) {
 		int lessonTeacherId = lessonRepository.findByLessonIdAndLessonStatus(lessonId,1).getTeacherId();
-		int thisTeacherId = tools.getTeacherId();
+		int thisTeacherId = getTeacherId(request);
 		if(lessonTeacherId!=thisTeacherId) return "你在尝试删除谁的课程?";
 		lessonRepository.delete(lessonId);
 		return "删除成功";
@@ -104,7 +117,7 @@ public class PubLessonController {//留着
 	//修改:localhost:8080/pubLesson/modify?lessonId=1
 	@RequestMapping("/modify")
 	public String modify(HttpServletRequest request,int lessonId) throws IOException {
-		int thisTeacherId = tools.getTeacherId();
+		int thisTeacherId = getTeacherId(request);
 		int lessonTeacherId = lessonRepository.findByLessonIdAndLessonStatus(lessonId,1).getTeacherId();
 		if(lessonTeacherId!=thisTeacherId) {
 			return "不是你的课程不允许改";
@@ -170,8 +183,8 @@ public class PubLessonController {//留着
 	
 	//分页显示我报名的公开课:http://localhost:8011/pubLesson/myPubLesson?no=1
 	@RequestMapping("/myPubLesson")
-	public Page<PubLesson> myLesson(int no) {
-		int memberId = tools.getMemberId();//获取userId
+	public Page<PubLesson> myLesson(int no,HttpServletRequest request) {
+		int memberId = getMemberId(request);//获取userId
 		Pageable pageable = new PageRequest(no-1, 2);//稍微处理了一下
 		List<Integer> lessonIdList = myLessonsRepository.findMyLessons(memberId);
 		return lessonRepository.findByLessonIdInAndLessonStatus(lessonIdList,pageable,1);
@@ -187,7 +200,7 @@ public class PubLessonController {//留着
 	//分页显示我教的公开课http://localhost:8011/pubLesson/myPubLesson?no=1
 	@RequestMapping("/teachLesson")
 	public Page<PubLesson> teachLesson(HttpServletResponse response,HttpServletRequest request,int no) {
-		int teacherId = tools.getTeacherId();//获取teacherId
+		int teacherId = getTeacherId(request);//获取teacherId
 		if(teacherId!=-1) {
 			//System.out.println("teacherId="+teacherId);
 			Pageable pageable = new PageRequest(no-1, 2);//稍微处理了一下
@@ -207,8 +220,8 @@ public class PubLessonController {//留着
 	
 	//名称,讲师,分类多条件模糊查询,分页:localhost:8080/pubLesson/fuzzySearch?no=1&lessonName=java课1&teacher=教师1&lessonType=
 	@RequestMapping("/fuzzySearch")
-	public Page<PubLesson> fuzzySearch(int no,String lessonName,String teacher,String lessonType) {
-		if(!tools.isAdmin()) return null;
+	public Page<PubLesson> fuzzySearch(int no,String lessonName,String teacher,String lessonType,HttpServletRequest request) {
+		if(!isAdmin(request)) return null;
 		Pageable pageable = new PageRequest(no-1, 10);//稍微处理了一下
 		System.out.println(lessonName+"|"+teacher+"|"+lessonType);
 		return lessonRepository.findByLessonNameContainingAndTeacherContainingAndLessonTypeContaining(lessonName,teacher,lessonType,pageable);
@@ -216,16 +229,16 @@ public class PubLessonController {//留着
 	
 	//管理员关闭功能与恢复功能
 	@RequestMapping("/adminStatus")
-	public String adminStatus(int lessonStatus,@RequestParam("lessonId") List<Integer> lessonId) {
+	public String adminStatus(int lessonStatus,@RequestParam("lessonId") List<Integer> lessonId,HttpServletRequest request) {
 		System.out.println(lessonId);
-		if(!tools.isAdmin()) return null;
+		if(!isAdmin(request)) return null;
 		int n = lessonRepository.adminStatus(lessonStatus, lessonId);
 		return "处理了"+n+"条";
 	}
 	//管理员删除功能
 	@RequestMapping("/adminDel")
-	public String adminDel(@RequestParam("lessonId") List<Integer> lessonId) {
-		if(!tools.isAdmin()) return null;
+	public String adminDel(@RequestParam("lessonId") List<Integer> lessonId,HttpServletRequest request) {
+		if(!isAdmin(request)) return null;
 		int n = lessonRepository.adminDel(lessonId);
 		return "删除了"+n+"条";
 	}
@@ -236,5 +249,111 @@ public class PubLessonController {//留着
 		if(lessonRepository.lesStuNumAdd(lessonId)==1)
 		return "购买成功";
 		return "购买失败";
+	}
+	
+	//cookie,redis和session相关
+	//基础,从cookie中获取String
+	private String readCookie(String name,HttpServletRequest request) {
+		//System.out.println(response+"-----"+request);
+		Cookie[] cookies = request.getCookies();
+		if (null != cookies) {
+			for (Cookie cookie : cookies) {
+				if(cookie.getName().equals(name)) {
+					System.out.println("cookie的值:"+cookie.getValue());
+					return cookie.getValue();
+				}
+			}
+		}
+		return null;
+	}
+	//基础,从redis中获取user
+	private User getUser(String username) {
+		ValueOperations opsForValue = redisTemplate.opsForValue();
+		JSONObject object = (JSONObject) opsForValue.get(username);
+		if(object!=null) {
+			User user = object.toJavaObject(User.class);
+			System.out.println("user--"+user);
+			return user;
+			//return ((JSONObject) redisTemplate.opsForValue().get(username)).toJavaObject(User.class);
+		}
+		return null;
+	}
+	//基础,获取userId
+	private int getUserId(HttpServletRequest request) {
+		//addCookie();//创建cookie,测试用
+		String username = readCookie("name",request);//从cookie中获取-username为"user"
+		if(username!=null) {
+			User user = getUser(username);
+			if(user!=null) {
+				System.out.println("userId"+user.getUserId());
+				return user.getUserId();//从redis中获取-userId为2
+			}
+		}
+		return -1;
+	}
+	//获取session中的教师信息,没有则尝试从数据库中查询,并添加到session中
+	private Teacher getSessionTeacher(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		Teacher teacher = (Teacher) session.getAttribute("teacher");
+		if(teacher==null) {
+			int userId = getUserId(request);
+			teacher = teacherRepository.findByUserId(userId);
+			if(teacher!=null) {
+				session.setAttribute("teacher", teacher);
+			}
+		}
+		System.out.println("teacher--"+teacher);
+		return teacher;
+	}
+	//获取session中的会员信息,没有则尝试从数据库中查询,并添加到session中
+	private Member getSessionMember(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		Member member = (Member) session.getAttribute("member");
+		if(member==null) {
+			int userId = getUserId(request);
+			member = memberRepository.findByUserId(userId);
+			if(member!=null) session.setAttribute("member", member);
+		}
+		System.out.println("member--"+member);
+		return member;
+	}
+	//偷懒,获取teacherId
+	public int getTeacherId(HttpServletRequest request) {
+		if(isLanding(request)) {
+			Teacher teacher = getSessionTeacher(request);
+			if(teacher!=null) {
+				System.out.println("teacherId"+teacher.getTeacherId());
+				return teacher.getTeacherId();
+			}
+		}
+		return -1;
+	}
+	//偷懒,获取memberId
+	public int getMemberId(HttpServletRequest request) {
+		Member member = getSessionMember(request);
+		if(isLanding(request)) {
+			if(member!=null) {
+				System.out.println("memberId"+member.getMemberId());
+				return member.getMemberId();
+			}
+		}
+		return -1;
+	}
+	//偷懒,看是否登陆(这里的name后期要改)
+	public boolean isLanding(HttpServletRequest request) {
+		//addCookie();//创建cookie,临时方法
+		String username = readCookie("user",request);//从cookie中获取-username为"user"
+		System.out.println(username!=null&&getUser(username)!=null);
+		return username!=null&&getUser(username)!=null;//两个都非空则处于登录状态
+	}
+	//偷懒,看是否管理员(这里的name2后期要改)
+	public boolean isAdmin(HttpServletRequest request) {
+		//addCookie2();//创建cookie,临时方法
+		String adminname = readCookie("user",request);
+		System.out.println("adminname"+adminname);
+		System.out.println("getUser"+getUser(adminname));
+		boolean isAdmin = adminname!=null&&getUser(adminname)!=null;
+		System.out.println("是管理员吗?"+isAdmin);
+		return isAdmin;//两个都非空是管理员
 	}
 }
